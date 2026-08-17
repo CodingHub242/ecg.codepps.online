4899cc43dce4300c5e9ef5b95ff0483a8af93896
CodingHub242 <codinghub242@gmail.com>
2026-08-17 19:50:47 +0000

    Create DeletedEmployee model for deleted employees

---
+++ app/Models/DeletedEmployee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DeletedEmployee extends Model
+{
+    protected $table = 'deleted_employees';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'employee_id',
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'deleted_at' => 'datetime',
+    ];
+}
