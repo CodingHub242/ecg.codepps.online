bcb746afc7c2109f488af91a7ad31caff141fc99
CodingHub242 <codinghub242@gmail.com>
2026-08-17 22:04:00 +0000

    Uncomment Controller import in EmployeeController

--- app/Http/Controllers/api/EmployeeController.php
+++ app/Http/Controllers/api/EmployeeController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Http\Controllers\api;
 
-//use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
+
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
